505e718544a28b4b120c85e17c94ac09911a13cb
colinrcummings <colinrcummings@gmail.com>
2018-09-18 14:32:11 -0400

    update technologyy and about

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // configs
-var sliiideConfig = {
+const sliiideConfig = {
   toggle: '#menu-toggle',
   exit_selector: '#menu-exit',
   animation_duration: '0.5s',
@@ -8,33 +8,37 @@ var sliiideConfig = {
   body_slide: false,
   no_scroll: true
 };
-var projects;
-var techArr = [
-  {id: 'javascript', file: 'javascript.svg', color: '#f7df1e', name: 'JavaScript'},
-  {id: 'react', file: 'react.svg', color: '#53c1de', name: 'React'},
-  {id: 'redux', file: 'redux.svg', color: '#764abc', name: 'Redux'},
-  {id: 'bootstrap', file: 'bootstrap.svg', color: '#563d7c', name: 'Bootstrap'},
-  {id: 'd3', file: 'd3.svg', color: '#f68e45', name: 'D3'},
-  {id: 'node', file: 'node.svg', color: '#8cc84b', name: 'Node'},
-  {id: 'webpack', file: 'webpack.svg', color: '#1c78c0', name: 'Webpack'},
-  {id: 'gulp', file: 'gulp.svg', color: '#cf4647', name: 'Gulp'},
-  {id: 'jasmine', file: 'jasmine.svg', color: '#8a4182', name: 'Jasmine'}
+let projects;
+const techArr = [
+  {
+    id: 'javascript',
+    file: 'javascript.svg',
+    color: '#f7df1e',
+    name: 'JavaScript'
+  },
+  { id: 'node', file: 'node.svg', color: '#8cc84b', name: 'Node' },
+  { id: 'webpack', file: 'webpack.svg', color: '#1c78c0', name: 'Webpack' },
+  { id: 'react', file: 'react.svg', color: '#53c1de', name: 'React' },
+  { id: 'd3', file: 'd3.svg', color: '#f68e45', name: 'D3' },
+  { id: 'jest', file: 'jest.svg', color: '#c63d14', name: 'Jest' },
+  { id: 'cypress', file: 'cypress.svg', color: '#fbfbfb', name: 'Cypress' }
 ];
-var lastTech;
+let lastTech;
 
-
-//document ready function
-$( document ).ready(function(){
-  //load project data and draw web projects
+// document ready function
+$(document).ready(function() {
+  // load project data and draw web projects
   d3.json('/data/projects.json', function(data) {
     projects = data.projects;
     drawProjects('web');
   });
-  //initiate sliiide
+  // initiate sliiide
   $('#menu').sliiide(sliiideConfig);
-  //initiate splash typed
+  // initiate splash typed
   $('#splash-typed-text').typed({
-    strings: techArr.map(function(d){return d.name; }),
+    strings: techArr.map(function(d) {
+      return d.name;
+    }),
     typeSpeed: 100,
     startDelay: 20,
     backSpeed: 35,
@@ -44,9 +48,9 @@ $( document ).ready(function(){
       changeTech(curString);
     }
   });
-  //initiate contact typed
+  // initiate contact typed
   $('#contact-typed-text').typed({
-    strings: ['onnect...','hat...','ollaborate...','ode...','reate...'],
+    strings: ['onnect...', 'hat...', 'ollaborate...', 'ode...', 'reate...'],
     typeSpeed: 100,
     startDelay: 20,
     backSpeed: 35,
@@ -55,37 +59,42 @@ $( document ).ready(function(){
   });
 });
 
-
 // project functions
-function drawProjects (projType) {
+function drawProjects(projType) {
   // set projects container min-height
-  var projContainerHeight = $('#project-container').height();
+  const projContainerHeight = $('#project-container').height();
   $('#project-container').css('min-height', projContainerHeight);
   // remove outgoing projects
   $('.project').remove();
   // add incoming projects
-  var typeProjects = projects.filter(function(d){ return d.type === projType; });
-  typeProjects.forEach(function(d){
+  const typeProjects = projects.filter(function(d) {
+    return d.type === projType;
+  });
+  typeProjects.forEach(function(d) {
     $('#project-container').append(
       '<div class="col-md-6 project">' +
-        '<img class="img-responsive center-block project-img" src="/images/projects/'
-        + d.image + '" title="Click for project details" onclick="showProjectModal('
-        + d.key + ')">'
-      + '</div>'
+        '<img class="img-responsive center-block project-img" src="/images/projects/' +
+        d.image +
+        '" title="Click for project details" onclick="showProjectModal(' +
+        d.key +
+        ')">' +
+        '</div>'
     );
   });
   // update buttons
   $('.work-btn').removeClass('work-btn-selected');
   $('#' + projType + '-btn').addClass('work-btn-selected');
   // reset project container min-height after a quarter of a second
-  setTimeout(function(){
+  setTimeout(function() {
     $('#project-container').css('min-height', '0');
   }, 250);
 }
 
-function showProjectModal (projKey) {
+function showProjectModal(projKey) {
   // update modal content
-  var projData = projects.filter(function(d){ return d.key === projKey; })[0];
+  const projData = projects.filter(function(d) {
+    return d.key === projKey;
+  })[0];
   $('#proj-modal-name').text(projData.name);
   $('#proj-modal-img').attr('src', 'images/projects/' + projData.image);
   $('#proj-modal-release').text(projData.release);
@@ -96,21 +105,23 @@ function showProjectModal (projKey) {
   $('#project-modal').modal();
 }
 
-
 // technology functions
-function changeTech (techIndex) {
-  var incomingTech = techArr[techIndex];
+function changeTech(techIndex) {
+  const incomingTech = techArr[techIndex];
   $(lastTech).addClass('slideOutLeft animated');
-  setTimeout(function(){
+  setTimeout(function() {
     $(lastTech).remove();
     lastTech = '#' + incomingTech.id;
   }, 500);
   $('#splash-typed-text').css('color', incomingTech.color);
-  $('#tech-svg').append('<img src="/images/tech-svg/' + incomingTech.file
-    + '" id="' + incomingTech.id + '" class="tech-img slideInRight animated">'
+  $('#tech-svg').append(
+    '<img src="/images/tech-svg/' +
+      incomingTech.file +
+      '" id="' +
+      incomingTech.id +
+      '" class="tech-img slideInRight animated">'
   );
 }
-
 
 // waypoints
 $('.wp1').waypoint(function() {
@@ -123,11 +134,10 @@ $('.wp2').waypoint(function() {
   $('.wp2-effect-2').addClass('animated fadeInUp');
 });
 
-
 // header background scroll
 $(function() {
   $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
+    const scroll = $(window).scrollTop();
     if (scroll >= 20) {
       $('.fixed-header').addClass('has-background fixed-header-sm');
     } else {
@@ -136,19 +146,21 @@ $(function() {
   });
 });
 
-
 // smooth scroll
 $('a[href*=#]:not([href=#])').click(function() {
   if (
-      location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
-      && location.hostname === this.hostname
-    ) {
-    var target = $(this.hash);
+    location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+    location.hostname === this.hostname
+  ) {
+    const target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
     if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top
-      }, 1200);
+      $('html,body').animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1200
+      );
       return false;
     }
   }
