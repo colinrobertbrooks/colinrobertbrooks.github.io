c727e07fa30c0df19f727f64184f7b5c4f482730
Colin Brooks <colinrobertbrooks@gmail.com>
2021-02-05 15:29:27 -0500

    remove waypoints

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const sliiideConfig = {
   place: "right",
   animation_curve: "cubic-bezier(0.54, 0.01, 0.57, 1.03)",
   body_slide: false,
-  no_scroll: true
+  no_scroll: true,
 };
 let projects;
 const techArr = [
@@ -14,13 +14,13 @@ const techArr = [
     id: "javascript",
     file: "javascript.svg",
     color: "#f7df1e",
-    name: "JavaScript"
+    name: "JavaScript",
   },
   {
     id: "typescript",
     file: "typescript.svg",
     color: "#007acc",
-    name: "TypeScript"
+    name: "TypeScript",
   },
   { id: "node", file: "node.svg", color: "#8cc84b", name: "Node" },
   { id: "webpack", file: "webpack.svg", color: "#1c78c0", name: "Webpack" },
@@ -28,14 +28,14 @@ const techArr = [
   { id: "graphql", file: "graphql.svg", color: "#E534AB", name: "GraphQL" },
   { id: "d3", file: "d3.svg", color: "#f68e45", name: "D3" },
   { id: "jest", file: "jest.svg", color: "#c63d14", name: "Jest" },
-  { id: "cypress", file: "cypress.svg", color: "#fbfbfb", name: "Cypress" }
+  { id: "cypress", file: "cypress.svg", color: "#fbfbfb", name: "Cypress" },
 ];
 let lastTech;
 
 // document ready function
-$(document).ready(function() {
+$(document).ready(function () {
   // load project data and draw web projects
-  d3.json("/data/projects.json", function(data) {
+  d3.json("/data/projects.json", function (data) {
     projects = data.projects;
     drawProjects("web");
   });
@@ -43,7 +43,7 @@ $(document).ready(function() {
   $("#menu").sliiide(sliiideConfig);
   // initiate splash typed
   $("#splash-typed-text").typed({
-    strings: techArr.map(function(d) {
+    strings: techArr.map(function (d) {
       return d.name;
     }),
     typeSpeed: 100,
@@ -51,9 +51,9 @@ $(document).ready(function() {
     backSpeed: 35,
     backDelay: 2000,
     loop: true,
-    preStringTyped: function(curString) {
+    preStringTyped: function (curString) {
       changeTech(curString);
-    }
+    },
   });
   // initiate contact typed
   $("#contact-typed-text").typed({
@@ -62,7 +62,7 @@ $(document).ready(function() {
     startDelay: 20,
     backSpeed: 35,
     backDelay: 2000,
-    loop: true
+    loop: true,
   });
 });
 
@@ -74,10 +74,10 @@ function drawProjects(projType) {
   // remove outgoing projects
   $(".project").remove();
   // add incoming projects
-  const typeProjects = projects.filter(function(d) {
+  const typeProjects = projects.filter(function (d) {
     return d.type === projType;
   });
-  typeProjects.forEach(function(d) {
+  typeProjects.forEach(function (d) {
     $("#project-container").append(
       '<div class="col-md-6 project">' +
         '<img class="img-responsive center-block project-img" src="/images/projects/' +
@@ -92,14 +92,14 @@ function drawProjects(projType) {
   $(".work-btn").removeClass("work-btn-selected");
   $("#" + projType + "-btn").addClass("work-btn-selected");
   // reset project container min-height after a quarter of a second
-  setTimeout(function() {
+  setTimeout(function () {
     $("#project-container").css("min-height", "0");
   }, 250);
 }
 
 function showProjectModal(projKey) {
   // update modal content
-  const projData = projects.filter(function(d) {
+  const projData = projects.filter(function (d) {
     return d.key === projKey;
   })[0];
   $("#proj-modal-name").text(projData.name);
@@ -116,7 +116,7 @@ function showProjectModal(projKey) {
 function changeTech(techIndex) {
   const incomingTech = techArr[techIndex];
   $(lastTech).addClass("slideOutLeft animated");
-  setTimeout(function() {
+  setTimeout(function () {
     $(lastTech).remove();
     lastTech = "#" + incomingTech.id;
   }, 500);
@@ -130,20 +130,9 @@ function changeTech(techIndex) {
   );
 }
 
-// waypoints
-$(".wp1").waypoint(function() {
-  $(".wp1-effect-1").addClass("animated fadeInDown");
-  $(".wp1-effect-2").addClass("animated fadeIn");
-  $(".wp1-effect-3").addClass("animated fadeInUp");
-});
-$(".wp2").waypoint(function() {
-  $(".wp2-effect-1").addClass("animated fadeIn");
-  $(".wp2-effect-2").addClass("animated fadeInUp");
-});
-
 // header background scroll
-$(function() {
-  $(window).scroll(function() {
+$(function () {
+  $(window).scroll(function () {
     const scroll = $(window).scrollTop();
     if (scroll >= 20) {
       $(".fixed-header").addClass("has-background fixed-header-sm");
@@ -154,7 +143,7 @@ $(function() {
 });
 
 // smooth scroll
-$("a[href*=#]:not([href=#])").click(function() {
+$("a[href*=#]:not([href=#])").click(function () {
   if (
     location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") &&
     location.hostname === this.hostname
@@ -164,7 +153,7 @@ $("a[href*=#]:not([href=#])").click(function() {
     if (target.length) {
       $("html,body").animate(
         {
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top,
         },
         1200
       );
